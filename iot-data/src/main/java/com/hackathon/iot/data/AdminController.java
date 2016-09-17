e06c3c2085626b234e23d301c2f64605a84d847b
James Eschner <jae226@cornell.edu>
2016-09-17 14:05:59 -0400

    add delete all endpoint

---
+++ iot-data/src/main/java/com/hackathon/iot/data/AdminController.java
@@ -0,0 +1,28 @@
+package com.hackathon.iot.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/admin")
+public class AdminController {
+
+    private DataPointRepository dataPointRepository;
+
+    @Autowired
+    public AdminController(DataPointRepository dataPointRepository) {
+        this.dataPointRepository = dataPointRepository;
+    }
+
+    @RequestMapping(value = "/deleteAll", method = POST)
+    public void deleteAll() {
+        dataPointRepository.deleteAll();
+    }
+
+}
